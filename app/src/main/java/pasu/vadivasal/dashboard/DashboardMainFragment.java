b390525a861853a1ab5e635b0d52fd2cd27eb768
Nagarajan S <nagarajan929@gmail.com>
2018-02-23 22:58:45 +0530

    Temp changes

--- app/src/main/java/pasu/vadivasal/dashboard/DashboardMainFragment.java
+++ app/src/main/java/pasu/vadivasal/dashboard/DashboardMainFragment.java
@@ -123,7 +123,6 @@ public static boolean versionAlertShown;
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-//                String value = dataSnapshot.getValue(String.class);
                 progressBar.setVisibility(View.GONE);
                 Log.d(TAG, "Value is: " + dataSnapshot.getValue().toString());
                 DashBoardData datav = dataSnapshot.getValue(DashBoardData.class);
